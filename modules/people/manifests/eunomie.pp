09bb9b9b0594b6fcd91e10e9462f4ebd64faa12e
CrEv <yves.brissaud@gmail.com>
2013-10-05 22:17:01 +0200

    Add git configuration

--- modules/people/manifests/eunomie.pp
+++ modules/people/manifests/eunomie.pp
@@ -58,4 +58,58 @@ class people::eunomie {
     $wlt:      source => 'eunomie/wlt', require => File[$perso];
   }
 
+  git::config::global {
+    'user.name':     value => 'Yves Brissaud';
+    'user.email':    value => 'yves@brissaud.name';
+
+    'color.ui':      value => 'true';
+
+    # so common alias
+    'alias.st':      value => 'status';
+    'alias.ci':      value => 'commit';
+    'alias.br':      value => 'branch';
+    'alias.co':      value => 'checkout';
+    'alias.df':      value => 'diff';
+    'alias.pr':      value => 'pull --rebase';
+
+    # log
+    # clean, oneline with graph
+    'alias.oneline': value => 'log --pretty=oneline --abbrev-commit --graph';
+    'alias.lc':      value => 'log ORIG_HEAD.. --stat --no-merges';
+    'alias.new':     value => '!sh -c \'git log $1@{1}..$1@{0} "$@"\'';
+    # outgoing
+    'alias.lout':    value => 'log --pretty=oneline --abbrev-commit --graph @{u}..';
+    'alias.out':     value => '!git fetch && git lout';
+    # incoming
+    'alias.lin':     value => 'log --pretty=oneline --abbrev-commit --graph ..@{u}';
+    'alias.in':      value => '!git fetch && git log --pretty=oneline --abbrev-commit --graph ..@{u}';
+
+    # diff
+    'alias.dic':     value => 'diff --cached';
+    'alias.diffstat': value => 'diff --stat';
+
+    'alias.undo':     value => 'reset --soft HEAD^';
+
+    # add modified files
+    'alias.addm':     value => '!git-ls-files -m -z | xargs -0 git-add && git status';
+    # add unknown files
+    'alias.addu':     value => '!git-ls-files -o --exclude-standard -z | xargs -0 git-add && git status';
+    # delete files marked as deleted
+    'alias.rmm':      value => '!git ls-files -d -z | xargs -0 git-rm && git status';
+
+    # server files
+    'alias.serve':    value => 'daemon --reuseaddr --verbose  --base-path=. --export-all ./.git';
+
+    # merge management
+    # $ git edit-unmerged
+    # $ ... edit ...
+    # $ ... test ...
+    # $ git add-unmerged
+    # $ git commit  # or git rebase --continue or whatever
+    'alias.edit-unmerged': value => '"!f() { git ls-files --unmerged | cut -f2 | sort -u ; }; sub `f`"';
+    'alias.add-unmerged':  value => '"!f() { git ls-files --unmerged | cut -f2 | sort -u ; }; git add `f`"';
+
+    # add github pull requests
+    'alias.pullify':       value => 'config --add remote.origin.fetch \'+refs/pull/*/head:refs/remotes/origin/pr/*\'';
+  }
 }
