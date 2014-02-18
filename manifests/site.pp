93a972bb0037d5d46122588c2811c0555e4c2a72
Yves Brissaud <yves@brissaud.name>
2014-02-18 13:37:45 +0100

    remove old ruby and node

--- manifests/site.pp
+++ manifests/site.pp
@@ -64,8 +64,6 @@ node default {
   }
 
   # node versions
-  include nodejs::v0_6
-  include nodejs::v0_8
   include nodejs::v0_10
 
   class { 'nodejs::global':
@@ -73,11 +71,9 @@ node default {
   }
 
   # default ruby versions
-  include ruby::1_8_7
-  include ruby::1_9_2
   include ruby::1_9_3
   include ruby::2_0_0
-  
+
   class { 'ruby::global':
     version => '1.9.3'
   }
